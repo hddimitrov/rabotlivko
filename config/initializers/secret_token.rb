8650249b72031a8edd8221c9b133f1f7c784402d
Hristo Peev <hristo.sv.peev@gmail.com>
2013-06-22 11:51:17 +0300

    initial setup of rails rabotlivko project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rabotlivko::Application.config.secret_token = '35873cb43a5d7df8af9745da42bb996142f1f4923c0d960569fe5aea87fff3a413c379c5cb021015f44e8bd8250a3bd8dd77e4270b5aac3c80ab1358a037fc40'
