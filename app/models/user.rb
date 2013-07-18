b137f576f4ac6d66df9173621c809fecdf32ca16
hristopeev <hristo.sv.peev@gmail.com>
2013-07-18 16:40:04 +0300

    and the model has only name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :first_name, :last_name
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
   has_many :adverts
 end
