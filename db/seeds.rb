11886736fe5c5eefa99e0693771bfc09eacabbc0
hddimitrov <hddimitrov@gmail.com>
2013-08-12 19:28:40 +0300

    add categories seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.find_or_create_by_title('Домакинска работа')
+Category.find_or_create_by_title('Монтажни')
+Category.find_or_create_by_title('Хамалски')
+Category.find_or_create_by_title('Ремонт')
+Category.find_or_create_by_title('Транспорт')
+Category.find_or_create_by_title('Пазаруване')
+Category.find_or_create_by_title('Промотиране')
+Category.find_or_create_by_title('Дом. любимци')
+Category.find_or_create_by_title('Градина')
+Category.find_or_create_by_title('Козметични')
+Category.find_or_create_by_title('Писане')
+Category.find_or_create_by_title('Преводи')
