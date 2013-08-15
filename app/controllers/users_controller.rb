2129647f6e041815f702ea1b1d0b308f46bc5e15
hpeev <hristo.sv.peev@gmail.com>
2013-08-15 22:55:24 +0300

    show archived wanted ads in user profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
     adverts = @user.adverts
     want_ads = @user.want_ads
 
-    @adverts_active = adverts.select {|advert| advert.q_draft == false }
-    @wanted_ads_active = want_ads.select {|want_ad| want_ad.q_draft == false }
+    @adverts_active     = adverts.select {|advert| advert.q_draft == false }
+    @advert_drafts      = adverts.select {|advert| advert.q_draft }
 
-    @advert_drafts    = adverts.select {|advert| advert.q_draft }
-    @wanted_ad_drafts = want_ads.select {|want_ad| want_ad.q_draft }
+    @wanted_ads_active  = want_ads.select {|want_ad| want_ad.q_draft == false && want_ad.deadline >= Date.today}
+    @wanted_ads_archive = want_ads.select {|want_ad| want_ad.deadline < Date.today }
+    @wanted_ad_drafts   = want_ads.select {|want_ad| want_ad.q_draft }
   end
 
   def profile_master
