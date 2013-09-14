4c49a1f343de7f156254f70386c6a0d93d7546a4
hpeev <hristo.sv.peev@gmail.com>
2013-09-14 18:17:06 +0300

    test some smtp settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,10 +55,10 @@ Rabotlivko::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
     port: 587,                           # ports 587 and 2525 are also supported with STARTTLS
-    enable_starttls_auto:  true,         # detects and uses STARTTLS
+    # enable_starttls_auto:  true,         # detects and uses STARTTLS
     user_name: ENV['MANDRILL_USERNAME'], # "MANDRILL_USERNAME",
     password: ENV['MANDRILL_PASSWORD'],  # "MANDRILL_PASSWORD", # SMTP password is any valid API key
-    authentication: 'login',             # Mandrill supports 'plain' or 'login'
+    # authentication: 'login',             # Mandrill supports 'plain' or 'login'
     domain: 'rabotazaden.com',           # your domain to identify your server when connecting
   }
 
